0db9c9f422e2aed855922140781cb2f1895a6bb0
Dipesh <dpkshn279@gmail.com>
2025-09-25 11:43:50 +0100

    frontend push

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,9 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Replace USERNAME, PASSWORD, HOST, PORT, DB_NAME with your Postgres values
-SQLALCHEMY_DATABASE_URL = "postgresql://USERNAME:PASSWORD@localhost:5432/DB_NAME"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./star_library.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
